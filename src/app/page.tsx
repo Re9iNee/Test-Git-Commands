82009de76cf672fba2516885a62a625d55885696
Re9iNee <attarzadeh76@gmail.com>
2024-10-22 15:09:50 +0330

    without api token

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 
+const MY_SECRET_API_TOKEN  = process.env.MY_SECRET_API_TOKEN;
+
+
 export default function Home() {
+  const data = await fetch("https://example.com/api", { method: "POST", authorization: MY_SECRET_API_TOKEN })
+  
+  
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
